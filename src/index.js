f2c8b0edf8ba30893bf25e1d1fa193269257bd19
Francesco <CiccioFr@tiscali.it>
2022-11-21 14:41:10 +0100

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
+//import Home from './components/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+    <Home />
+  </BrowserRouter>
   </React.StrictMode>
 );
 
